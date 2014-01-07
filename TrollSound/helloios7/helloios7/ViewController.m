a551cb93e471a2d9af27553a6b7142251e035e59
Sebastian <stellez@ohkasystems.com>
2014-01-07 13:14:59 -0600

    resubiendo

--- TrollSound/helloios7/helloios7/ViewController.m
+++ TrollSound/helloios7/helloios7/ViewController.m
@@ -32,15 +32,20 @@ NSArray *arreglo;
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //Creando objetos para la fuente customizada
     UIFont *fontCustom = [UIFont fontWithName:@"Fluoxetine" size:25.0f];
     UIFont *fontCustom2 = [UIFont fontWithName:@"Fluoxetine" size:15.0f];
     
+    //asignando fuente
     trollLabel.font = fontCustom;
     wrongLabel.font = fontCustom2;
     badumLabel.font = fontCustom2;
     clampLabel.font = fontCustom2;
     lawLabel.font =fontCustom2;
     
+    
+    //asignando textos a los labels
     trollLabel.text = @"TROLL Sound";
     wrongLabel.text = @"wrong";
     badumLabel.text = @"ba dum tss";
@@ -49,11 +54,14 @@ NSArray *arreglo;
     
     trollLabel.transform = CGAffineTransformMakeRotation((-10 * M_PI)/100);
     
+    //asignando los PATH de los sonidos para despues ser asignados a un objeto player y asi ser reproducidos
+    
     lawUrl = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"doink_doink" ofType:@"mp3"]];
     badumUrl = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"BaDumTss" ofType:@"mp3"]];
     clampUrl = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"aplausos" ofType:@"wav"]];
     wrongUrl = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"wrong" ofType:@"wav"]];
     
+    //este arreglo nos sirve para que mas adelante con la ayuda de un random accesemos al PATH del sonido que se reproducira aleatoriamente
     arreglo = [[NSArray alloc] initWithObjects:lawUrl,badumUrl,clampUrl,wrongUrl, nil];
     
 }
@@ -87,9 +95,8 @@ NSArray *arreglo;
 - (IBAction)troll:(id)sender {
     
     int randomNum = rand()%4;
+    
     NSLog(@"dime el numero random %i",randomNum);
-    //NSInteger idx = [arreglo objectAtIndex:randomNum];
-    //NSLog(@"dime el url %@", [arreglo objectAtIndex:1]);
     player = [[AVAudioPlayer alloc] initWithContentsOfURL:[arreglo objectAtIndex:randomNum] error:nil];
     [player play];
     
